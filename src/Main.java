6acaf414da4676a4b6ff2d7e2dee9b760d534307
bhumiii-codes <bhumikaaa31@gmail.com>
2026-03-10 14:30:02 +0530

    UC1: Application Entry Point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+/**
+ * Entry point for Hotel Booking Management System
+ * @author Bhumika
+ * @version 1.0
+ */
 public class Main {
+
     public static void main(String[] args) {
+
+        System.out.println("=================================");
         System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("=================================");
+
+        System.out.println("Application started successfully");
+
     }
 }
